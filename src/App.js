57f0b2cc0ff6de18f464838440d4659c3613aefb
tspmwant <tspm.wantoro@gmail.com>
2020-06-23 16:06:33 +0700

    some css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import HelloWorld from "./Components/HelloWorld";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
