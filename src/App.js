7f6182a9727a1ce2af12d85e92965c6154dff373
tspmwant <tspm.wantoro@gmail.com>
2020-06-23 14:29:03 +0700

    tambah footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HelloWorld from "./Components/HelloWorld";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <HelloWorld name="Cobain" />
+      <Footer />
     </div>
   );
 }
