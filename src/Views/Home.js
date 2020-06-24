5c28dae76a3ad029a08b55ce017f8b2ee1eadf62
tspmwant <tspm.wantoro@gmail.com>
2020-06-24 16:56:50 +0700

    haous import

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Loader from "../Components/Loader";
-import Product from "./Product";
 import ProductCard from "../Components/ProductCard";
 import { useAxiosGet } from "../Hooks/HttpsRequests";
 
